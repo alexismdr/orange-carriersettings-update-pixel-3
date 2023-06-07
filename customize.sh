5f9233da24a7d5f33de7cf171d08892c13d5f0e1
Apern <32903418+apernouille@users.noreply.github.com>
2023-06-07 18:55:30 +0200

    Customization

---
+++ customize.sh
@@ -0,0 +1,6 @@
+#!/system/bin/sh
+
+  ui_print "***************************************"
+  ui_print "*  Orange F : Update CarrierSettings  *"
+  ui_print "*     from github.com/apernouille     *"
+  ui_print "***************************************"
